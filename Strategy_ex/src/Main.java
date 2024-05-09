a6c42bc7310174e3acd34c0adc614464e378b137
WalaceSilva03 <151690874+WalaceSilva03@users.noreply.github.com>
2024-05-08 22:26:06 -0300

    Strategy Teste
    Aprendendo o conceito de strategy

---
+++ Strategy_ex/src/Main.java
@@ -0,0 +1,15 @@
+public class Main {
+    public static void main(String[] args) {
+
+        Aritmetica calc = new Aritmetica();
+
+        Disciplina d = new Disciplina(calc);
+
+        d.setNome("COBOL");
+        d.setP1(7); d.setP2(7);
+        d.CalculaMedia();
+        System.out.println(
+                String.format("P1:%.2f P2:%.2f Media:%.2f Situacao: %s",
+                        d.getP1(),d.getP2(), d.getMedia(), d.getSituacao()));
+    }
+}
